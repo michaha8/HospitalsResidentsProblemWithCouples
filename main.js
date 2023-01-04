e657d76d7ce6f12a6509beac33f8d24a0d54e0c3
michael harush <michaelharush551@gmail.com>
2023-01-04 18:55:23 +0200

    update algorithm with list preference  hospitals

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ function hospitalResidents(residents, hospitals) {
     // Sort residents by preference in ascending order
     residents.sort((a, b) => a.preference - b.preference);
   
-    // Sort hospitals by capacity in descending order
-    hospitals.sort((a, b) => b.capacity - a.capacity);
+    // Sort hospitals by preference in ascending order
+    hospitals.sort((a, b) => a.preference - b.preference);
   
     for (const resident of residents) {
       if (resident.partner) {
@@ -31,7 +31,7 @@ function hospitalResidents(residents, hospitals) {
       if (!resident.assignedHospital) {
         resident.assignedHospital = '';
         for (const hospital of hospitals) {
-          if (hospital.capacity > 0) {
+          if (hospital.capacity > 0 && hospital.preferences.includes(resident.name)) {
             hospital.capacity -= 1;
             resident.assignedHospital = hospital.name;
             break;
@@ -46,20 +46,25 @@ function hospitalResidents(residents, hospitals) {
     }
   }
   
-  
 
-  const residents = [  { name: 'Alice', preference: 3, partner: '', preferences: ['Hospital A', 'Hospital B', 'Hospital C'] },
-  { name: 'Bob', preference: 2, partner: 'Eve', preferences: ['Hospital B', 'Hospital A', 'Hospital C'] },
-  { name: 'Eve', preference: 2, partner: 'Bob', preferences: ['Hospital C', 'Hospital A', 'Hospital B'] },
-  { name: 'Mallory', preference: 1, partner: '', preferences: ['Hospital C', 'Hospital A', 'Hospital B'] },
+  // Define the residents
+const residents = [  { name: 'Alice', preference: 3, partner: '', preferences: ['Hospital A', 'Hospital B', 'Hospital C'] },
+{ name: 'Bob', preference: 2, partner: '', preferences: ['Hospital B', 'Hospital A', 'Hospital C'] },
+{ name: 'Eve', preference: 1, partner: 'Mallory', preferences: ['Hospital C', 'Hospital A', 'Hospital B'] },
+{ name: 'Mallory', preference: 1, partner: 'Eve', preferences: ['Hospital C', 'Hospital A', 'Hospital B'] },
 ];
 
-const hospitals = [  { name: 'Hospital A', capacity: 2 },  { name: 'Hospital B', capacity: 1 },  { name: 'Hospital C', capacity: 2 },];
+// Define the hospitals
+const hospitals = [  { name: 'Hospital A', preference: 3, capacity: 2, preferences: ['Alice', 'Bob', 'Eve', 'Mallory'] },
+{ name: 'Hospital B', preference: 2, capacity: 3, preferences: ['Bob', 'Alice', 'Eve', 'Mallory'] },
+{ name: 'Hospital C', preference: 1, capacity: 3, preferences: ['Eve', 'Mallory', 'Alice', 'Bob'] },
+];
 
+// Assign the residents to hospitals
 hospitalResidents(residents, hospitals);
 
 // Output:
-// Alice has been assigned to Hospital A.
-// Bob has been assigned to Hospital B.
+// Alice has been assigned to Hospital B.
+// Bob has been assigned to Hospital A.
 // Eve has been assigned to Hospital C.
-// Mallory has been assigned to Hospital A.
+// Mallory has been assigned to Hospital C.
