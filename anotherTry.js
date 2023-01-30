bd52665cbbd59cd2e05c1b910933e87d39f3124a
michael harush <michaelharush551@gmail.com>
2023-01-30 16:17:51 +0200

    anotherTry + createRandomInput + TabuSearch

---
+++ anotherTry.js
@@ -0,0 +1,142 @@
+function stablePairing(interns, hospitals) {
+    // Initialize all interns and hospitals as unmatched
+    let internMatches = {};
+    let hospitalMatches = {};
+    for (let intern of interns) {
+        internMatches[intern.name] = null;
+    }
+    for (let hospital of hospitals) {
+        hospitalMatches[hospital.name] = [];
+    }
+
+    // While there are still unmatched interns
+    while (Object.values(internMatches).includes(null)) {
+        for (let intern of interns) {
+            // Skip matched interns
+            if (internMatches[intern.name] != null) {
+                continue;
+            }
+
+            // Propose to highest ranked unmatched hospital that has not yet rejected them and has available spots
+            for (let hospitalName of intern.preferences) {
+                let hospital = hospitals.find(h => h.name === hospitalName);
+                if (hospitalMatches[hospitalName].length < hospital.numberOfInterns) {
+                    hospitalMatches[hospitalName].push(intern.name);
+                    internMatches[intern.name] = hospitalName;
+                    break;
+                }
+            }
+
+            // If intern is part of a couple, and their partner is also unmatched, consider partner for the same hospital
+            if (intern.partner != null && internMatches[intern.partner] == null) {
+                for (let hospitalName of intern.preferences) {
+                    let hospital = hospitals.find(h => h.name === hospitalName);
+                    if (hospitalMatches[hospitalName].length < hospital.numberOfInterns) {
+                        hospitalMatches[hospitalName].push(intern.partner);
+                        internMatches[intern.partner] = hospitalName;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    // Check if a stable pairing was found
+    if (!Object.values(internMatches).includes(null)) {
+        console.log("A stable pairing was found.");
+    } else {
+        console.log("A stable pairing was not found.");
+    }
+
+    // Output the results of the pairing
+    console.log("Intern matches:", internMatches);
+    console.log("Hospital matches:", hospitalMatches);
+}
+
+
+// function createRandomInput(numInterns, numCouples, numHospitals) {
+//     // Create list of interns
+//     let interns = [];
+//     let names = ["Alice", "Bob", "Charlie", "David", "Eve", "Frank", "Grace", "Henry", "Iris", "Jack"];
+//     for (let i = 0; i < numInterns; i++) {
+//         let name = names[i];
+//         let partner = null;
+//         if (i < numCouples * 2) {
+//             partner = names[i ^ 1]; // Assigns partners to every other intern
+//         }
+//         let preferences = [];
+//         for (let j = 0; j < numHospitals; j++) {
+//             preferences.push("Hospital " + String.fromCharCode(65 + j));
+//         }
+//         interns.push({ name: name, partner: partner, preferences: preferences });
+//     }
+
+//     // Create list of hospitals
+//     let hospitals = [];
+//     for (let i = 0; i < numHospitals; i++) {
+//         let name = "Hospital " + String.fromCharCode(65 + i);
+//         let numberOfInterns = Math.floor(Math.random() * numInterns);
+//         let preferences = [];
+//         for (let j = 0; j < numInterns; j++) {
+//             preferences.push(names[j]);
+//         }
+//         hospitals.push({ name: name, numberOfInterns: numberOfInterns, preferences: preferences });
+//     }
+
+//     return { interns: interns, hospitals: hospitals };
+// }
+// let input = createRandomInput(5, 2, 3);
+// console.log(input);
+// stablePairing(input.interns, input.hospitals);
+
+function createRandomInput(numInterns, numCouples, numHospitals) {
+    // Create list of interns
+    let interns = [];
+    let partners = new Map();
+    for (let i = 0; i < numInterns; i++) {
+        let name = "Intern " + (i + 1);
+        let partner = null;
+        let preferences = [];
+        for (let j = 0; j < numHospitals; j++) {
+            preferences.push("Hospital " + String.fromCharCode(65 + j));
+        }
+        if(i < numCouples*2){
+            if(i%2 == 0){
+                partner = "Intern " + (i + 2);
+                partners.set(name, partner);
+                partners.set(partner,name);
+            }
+        }
+        preferences = shuffle(preferences);
+        interns.push({ name: name, partner: partners.get(name), preferences: preferences });
+    }
+
+    // Create list of hospitals
+    let hospitals = [];
+    for (let i = 0; i < numHospitals; i++) {
+        let name = "Hospital " + String.fromCharCode(65 + i);
+        let numberOfInterns = Math.max(Math.min(Math.floor(Math.random() * numInterns), numInterns),1);
+        let preferences = [];
+        for (let j = 0; j < numInterns; j++) {
+            preferences.push("Intern " + (j + 1));
+        }
+        preferences = shuffle(preferences);
+        hospitals.push({ name: name, numberOfInterns: numberOfInterns, preferences: preferences });
+    }
+
+    return { interns: interns, hospitals: hospitals };
+}
+
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+let input = createRandomInput(100, 25, 20);
+stablePairing(input.interns, input.hospitals);
+// let input = createRandomInput(5, 2, 3);
+ //console.log(input.interns);
+
--- main.js
+++ main.js
@@ -1,70 +1,255 @@
+// function hospitalResidents(residents, hospitals) {
+    
+//     // Randomly shuffle the Hospitals
+//     hospitals.sort(() => Math.random() - 0.5);
+  
+//     // Randomly shuffle the residents
+//     residents.sort(() => Math.random() - 0.5);
+  
+//     for (const resident of residents) {
+//       if (resident.partner) {
+//         // Try to assign both members of the couple to the same hospital
+//         const hospital = hospitals.find((h) => h.capacity >= 2);
+//         if (hospital) {
+//           hospital.capacity -= 2;
+//           resident.assignedHospital = hospital.name;
+//           continue;
+//         }
+//       }
+  
+//       // Try to assign the resident to their preferred hospital
+//       for (const hospital of hospitals) {
+//         if (hospital.capacity > 0 && resident.preferences.includes(hospital.name)) {
+//           hospital.capacity -= 1;
+//           resident.assignedHospital = hospital.name;
+//           break;
+//         }
+//       }
+//     }
+  
+//     // Assign any remaining residents to hospitals with remaining capacity
+//     for (const resident of residents) {
+//       if (!resident.assignedHospital) {
+//         resident.assignedHospital = '';
+//         for (const hospital of hospitals) {
+//           if (hospital.capacity > 0 && hospital.preferences.includes(resident.name)) {
+//             hospital.capacity -= 1;
+//             resident.assignedHospital = hospital.name;
+//             break;
+//           }
+//         }
+//       }
+//     }
+  
+//     // Print the name and assigned hospital of each resident
+//     for (const resident of residents) {
+//       console.log(`${resident.name} has been assigned to ${resident.assignedHospital}.`);
+//     }
+//   }
+
+
+function generateAssignments(numInterns, numCouples, numHospitals) {
+  // Generate an array of intern objects
+  const interns = [];
+  for (let i = 0; i < numInterns; i++) {
+      let intern = {};
+      intern.name = 'Intern' + i;
+      intern.partner = '';
+      intern.preferences = [];
+      for (let j = 0; j < numHospitals; j++) {
+          intern.preferences.push('Hospital' + j);
+      }
+      interns.push(intern);
+  }
+
+  // Assign partners to the specified number of couples
+  let partnerCount = 0;
+  while (partnerCount < numCouples) {
+      let intern1 = interns[Math.floor(Math.random() * numInterns)];
+      let intern2 = interns[Math.floor(Math.random() * numInterns)];
+      if (intern1.partner === '' && intern2.partner === '' && intern1.name !== intern2.name) {
+          intern1.partner = intern2.name;
+          intern2.partner = intern1.name;
+          partnerCount++;
+      }
+  }
+
+  // Generate an array of hospital objects
+  const hospitals = [];
+  for (let i = 0; i < numHospitals; i++) {
+      let hospital = {};
+      hospital.name = 'Hospital' + i;
+      hospital.capacity = Math.floor(Math.random() * numInterns/2);
+      hospital.preferences = [];
+      for (let j = 0; j < numInterns; j++) {
+          hospital.preferences.push('Intern' + j);
+      }
+      hospitals.push(hospital);
+  }
+  console.log(interns);
+  console.log(hospitals);
+  // Assign interns to hospitals
+  hospitalResidents(interns, hospitals);
+}
+
 function hospitalResidents(residents, hospitals) {
-    // Sort residents by preference in ascending order
-    residents.sort((a, b) => a.preference - b.preference);
-  
-    // Sort hospitals by preference in ascending order
-    hospitals.sort((a, b) => a.preference - b.preference);
-  
-    for (const resident of residents) {
-      if (resident.partner) {
-        // Try to assign both members of the couple to the same hospital
-        const hospital = hospitals.find((h) => h.capacity >= 2);
-        if (hospital) {
-          hospital.capacity -= 2;
-          resident.assignedHospital = hospital.name;
-          continue;
+  // Create an array for residents that have not been assigned
+  let unassignedResidents = [...residents];
+  let residentProposals = {}
+  let hospitalProposals = {}
+  for (const resident of residents) {
+    residentProposals[resident.name] = resident.preferences
+  }
+  for (const hospital of hospitals) {
+    hospitalProposals[hospital.name] = hospital.preferences
+  }
+  let iteration = 0;
+  const maxIterations = residents.length * hospitals.length;
+  while (unassignedResidents.length > 0 && iteration < maxIterations) {
+    for (const resident of unassignedResidents) {
+        let residentPreference = residentProposals[resident.name];
+        if(!residentPreference.length) continue;
+        let hospitalName = residentPreference.shift()
+        if(hospitalProposals[hospitalName].includes(resident.name) && hospitals.find(h => h.name === hospitalName).capacity > 0){
+          resident.assignedHospital = hospitalName
+          hospitalProposals[hospitalName] = hospitalProposals[hospitalName].filter(r => r !== resident.name)
+          hospitals.find(h => h.name === hospitalName).capacity--;
+          unassignedResidents = unassignedResidents.filter(r => r.name !== resident.name);
         }
-      }
-  
-      // Try to assign the resident to their preferred hospital
-      for (const hospital of hospitals) {
-        if (hospital.capacity > 0 && resident.preferences.includes(hospital.name)) {
-          hospital.capacity -= 1;
-          resident.assignedHospital = hospital.name;
-          break;
-        }
-      }
     }
-  
-    // Assign any remaining residents to hospitals with remaining capacity
-    for (const resident of residents) {
-      if (!resident.assignedHospital) {
-        resident.assignedHospital = '';
-        for (const hospital of hospitals) {
-          if (hospital.capacity > 0 && hospital.preferences.includes(resident.name)) {
-            hospital.capacity -= 1;
-            resident.assignedHospital = hospital.name;
-            break;
-          }
-        }
+    iteration++;
+}
+  if(unassignedResidents.length > 0){
+    console.log("The algorithm was not able to assign all the residents to a hospital")
+  }
+  // Print the name and assigned hospital of each resident
+  for (const resident of residents) {
+    console.log(`${resident.name} has been assigned to ${resident.assignedHospital}.`);
+  }
+
+  const stable = isStable(residents,hospitals)
+  console.log(`The pairing is ${stable? 'stable': 'not stable'}`)
+  const fair = isFair(residents,hospitals)
+  console.log(`The pairing is ${fair? 'Fair':'Not Fair'}`)
+  console.log(`Number of hospitals : ${hospitals.length}`)
+  console.log(`Number of residents : ${residents.length}`)
+  for(const resident of residents){
+    for(const resident2 of residents){
+      if(resident.partner===resident2.name&& resident.assignedHospital===resident2.assignedHospital){
+        console.log(`${resident.name} is partner of ${resident2.name} And they both assigned to ${resident.assignedHospital}\n`);
       }
-    }
-  
-    // Print the name and assigned hospital of each resident
-    for (const resident of residents) {
-      console.log(`${resident.name} has been assigned to ${resident.assignedHospital}.`);
     }
   }
+}
+
+function isStable(residents,hospitals){
+  let stable =true
+  for(const resident of residents){
+    if(resident.preferences.indexOf(resident.assignedHospital)>resident.preferences.indexOf(hospitals.find(h=>h.preferences.indexOf(resident.name)<h.preferences.indexOf(resident.assignedHospital)))){
+      stable=false;
+      break
+    }
+  }
+  return stable;
+  
+}
+function isFair(residents,hospitals){
+  let fair = true
+//Code to check for fairness goes here
+  return fair
+}
+
+//number 2
+function hospitalResidents(residents, hospitals) {
+  // Randomly shuffle the Hospitals
+  hospitals.sort(() => Math.random() - 0.5);
+
+  // Randomly shuffle the residents
+  residents.sort(() => Math.random() - 0.5);
+
+  // Create an array for residents that have not been assigned
+  let unassignedResidents = [...residents];
+  let residentProposals = {}
+  let hospitalProposals = {}
+  for (const resident of residents) {
+    residentProposals[resident.name] = resident.preferences
+  }
+  for (const hospital of hospitals) {
+    hospitalProposals[hospital.name] = hospital.preferences
+  }
+  let iteration = 0;
+  const maxIterations = residents.length * hospitals.length;
+  while (unassignedResidents.length > 0 && iteration < maxIterations) {
+    // Iteratively propose and accept matches
+    for (const resident of unassignedResidents) {
+      let residentPreference = residentProposals[resident.name];
+      if(!residentPreference.length) continue;
+      let hospitalName = residentPreference[Math.floor(Math.random() * residentPreference.length)]
+      if(hospitalProposals[hospitalName].includes(resident.name) && hospitals.find(h => h.name === hospitalName).capacity > 0){
+        resident.assignedHospital = hospitalName
+        hospitalProposals[hospitalName] = hospitalProposals[hospitalName].filter(r => r !== resident.name)
+        hospitals.find(h => h.name === hospitalName).capacity--;
+        unassignedResidents = unassignedResidents.filter(r => r.name !== resident.name);
+      }
+    }
+    iteration++;
+  }
+  if(unassignedResidents.length > 0){
+    console.log("The algorithm was not able to assign all the residents to a hospital")
+  }
+      // Print the name and assigned hospital of each resident
+      for (const resident of residents) {
+        console.log(`${resident.name} has been assigned to ${resident.assignedHospital}.`);
+      }
+  
+      // Check for stability
+      const stable = isStable(residents,hospitals)
+      console.log(`The pairing is ${stable? 'stable': 'not stable'}`)
+  
+      // Check for fairness
+      const fair = isFair(residents,hospitals)
+      console.log(`The pairing is ${fair? 'fair': 'not fair'}`)
+      let counterCoupelsInTheSameHospital=0;
+      for(const resident of residents){
+             for(const resident2 of residents){
+              if(resident.partner===resident2.name&& resident.assignedHospital===resident2.assignedHospital){
+                counterCoupelsInTheSameHospital++;
+                console.log(`${resident.name} is partner of ${resident2.name} And they both assigned to ${resident.assignedHospital}\n`);
+              }
+            }
+          }
+          console.log(`Number of coupels in the same hospital:  ${counterCoupelsInTheSameHospital/2}`);
+    }
+
+
+
+
+
+
+
+
+
+    
+    function isStable(residents, hospitals){
+      let stable = true
+      for (const resident of residents) {
+        if(resident.preferences.indexOf(resident.assignedHospital)> resident.preferences.indexOf(hospitals.find(h=> h.preferences.indexOf(resident.name) < h.preferences.indexOf(resident.assignedHospital)))){
+          stable = false
+          break
+        }
+      }
+      return stable
+    }
+    function isFair(residents, hospitals){
+      let fair = true
+      //Code to check for fairness goes here
+      return fair
+    }
   
 
-  // Define the residents
-const residents = [  { name: 'Alice', preference: 3, partner: '', preferences: ['Hospital A', 'Hospital B', 'Hospital C'] },
-{ name: 'Bob', preference: 2, partner: '', preferences: ['Hospital B', 'Hospital A', 'Hospital C'] },
-{ name: 'Eve', preference: 1, partner: 'Mallory', preferences: ['Hospital C', 'Hospital A', 'Hospital B'] },
-{ name: 'Mallory', preference: 1, partner: 'Eve', preferences: ['Hospital C', 'Hospital A', 'Hospital B'] },
-];
 
-// Define the hospitals
-const hospitals = [  { name: 'Hospital A', preference: 3, capacity: 2, preferences: ['Alice', 'Bob', 'Eve', 'Mallory'] },
-{ name: 'Hospital B', preference: 2, capacity: 3, preferences: ['Bob', 'Alice', 'Eve', 'Mallory'] },
-{ name: 'Hospital C', preference: 1, capacity: 3, preferences: ['Eve', 'Mallory', 'Alice', 'Bob'] },
-];
 
-// Assign the residents to hospitals
-hospitalResidents(residents, hospitals);
 
-// Output:
-// Alice has been assigned to Hospital B.
-// Bob has been assigned to Hospital A.
-// Eve has been assigned to Hospital C.
-// Mallory has been assigned to Hospital C.
+
+
+generateAssignments(15, 5,4)
