877e27b2bc0e76e047dac8e0757ea1852a77d107
michael harush <michaelharush551@gmail.com>
2023-03-23 10:32:25 +0200

    try

--- tabuSearch.js
+++ tabuSearch.js
@@ -49,7 +49,9 @@ function tabuSearch(interns, hospitals, maxIterations, tabuSize) {
     let currentMatching = initialMatching;
     let currentStability = bestStability;
     let iteration = 0;
+    let i=1
     while (iteration < maxIterations) {
+        i++
         // Generate a list of candidate moves
         let candidateMoves = generateCandidateMoves(currentMatching, hospitals);
 
@@ -58,10 +60,10 @@ function tabuSearch(interns, hospitals, maxIterations, tabuSize) {
 
         // Choose the best move
         let bestMove = chooseBestMove(tabuMoves, currentMatching, hospitals);
-
+        
         // Update the current matching
         currentMatching = applyMove(currentMatching, bestMove);
-
+        console.log(`Change number ${i} `, currentMatching);
         // Update the tabu list
         tabuList.unshift(bestMove);
         if (tabuList.length > tabuSize) {
@@ -141,78 +143,82 @@ function createRandomInput(numInterns, numCouples, numHospitals) {
     let interns = [];
     let partners = new Map();
     for (let i = 0; i < numInterns; i++) {
-        let name = "Intern " + (i + 1);
-        let partner = null;
-        let preferences = [];
-        for (let j = 0; j < numHospitals; j++) {
-            preferences.push("Hospital " + String.fromCharCode(65 + j));
+      let name = "Intern " + (i + 1);
+      let partner = null;
+      let preferences = [];
+      for (let j = 0; j < numHospitals; j++) {
+        preferences.push("Hospital " + String.fromCharCode(65 + j));
+      }
+      if (i < numCouples * 2) {
+        if (i % 2 == 0) {
+          partner = "Intern " + (i + 2);
+          partners.set(name, partner);
+          partners.set(partner, name);
         }
-        if (i < numCouples * 2) {
-            if (i % 2 == 0) {
-                partner = "Intern " + (i + 2);
-                partners.set(name, partner);
-                partners.set(partner, name);
-            }
-        }
-        preferences = shuffle(preferences);
-        interns.push({ name: name, partner: partners.get(name), preferences: preferences });
+      }
+      shuffle(preferences); // Shuffle the preferences array
+      interns.push({ name: name, partner: partners.get(name), preferences: preferences });
     }
-
+  
     // Create list of hospitals
     let hospitals = [];
     for (let i = 0; i < numHospitals; i++) {
-        let name = "Hospital " + String.fromCharCode(65 + i);
-        let numberOfInterns = Math.floor(Math.random() * numInterns);
-        let preferences = [];
-        for (let j = 0; j < numInterns; j++) {
-            preferences.push("Intern " + (j + 1));
-        }
-        preferences = shuffle(preferences);
-        hospitals.push({ name: name, numberOfInterns: numberOfInterns, preferences: preferences });
+      let name = "Hospital " + String.fromCharCode(65 + i);
+      let numberOfInterns = Math.floor(Math.random() * numInterns)+2;
+      let preferences = [];
+      for (let j = 0; j < numInterns; j++) {
+        preferences.push("Intern " + (j + 1));
+      }
+      shuffle(preferences); // Shuffle the preferences array
+      hospitals.push({ name: name, numberOfInterns: numberOfInterns, preferences: preferences });
     }
-
+  
     // Initialize a random matching
     let matching = {};
     let freeInterns = [];
     for (let intern of interns) {
-        freeInterns.push(intern.name);
+      freeInterns.push(intern.name);
     }
     for (let hospital of hospitals) {
-        matching[hospital.name] = [];
+      matching[hospital.name] = [];
     }
     while (freeInterns.length > 0) {
-        let internName = freeInterns.pop();
-        let intern = interns.find(i => i.name === internName);
-        for (let hospitalName of intern.preferences) {
-            let hospital = hospitals.find(h => h.name === hospitalName);
-            if (matching[hospitalName].length < hospital.numberOfInterns) {
-                matching[hospitalName].push(internName);
-                break;
-            }
+      let internName = freeInterns.pop();
+      let intern = interns.find(i => i.name === internName);
+      for (let hospitalName of intern.preferences) {
+        let hospital = hospitals.find(h => h.name === hospitalName);
+        if (matching[hospitalName].length < hospital.numberOfInterns) {
+          matching[hospitalName].push(internName);
+          break;
         }
+      }
     }
-
+  
+    console.log(`First Matching ` ,matching);
     return { interns: interns, hospitals: hospitals, matching: matching };
-
-}
-
-function shuffle(array) {
+  }
+  
+  function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
     }
     return array;
-}
+  }
+  
 
-let input = createRandomInput(10, 2, 1);
-console.log('input');
-console.log(input);
+  let input = createRandomInput(50, 20, 17); // Generate preferences for three hospitals
+//   console.log('input');
+//   console.log(input);
+// for (let intern of input.interns) {
+//     console.log(`Intern ${intern.name} ${intern.preferences}\n`);
+//   }
 
 let result = tabuSearch(input.interns, input.hospitals, 100, 10);
 console.log('result');
 console.log(result)
 
 // Output the results of the pairing
-console.log("Intern matches:", JSON.stringify(result.matching));
-console.log("Hospital matches:", result.hospitals);
-console.log("Stability score:", result.stability);
+// console.log("Intern matches:", JSON.stringify(result.matching));
+// console.log("Hospital matches:", result.hospitals);
+// console.log("Stability score:", result.stability);
